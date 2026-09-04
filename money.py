d8abf40edd5025b90382a510c59cdfe837d67367
kidyang0404-blip <kidyang0404@gmail.com>
2026-09-04 12:01:47 +0900

    Add files via upload

---
+++ money.py
@@ -0,0 +1,29 @@
+a = ["1","2","3","4","0"] 
+b = ["기록 추가","전체 보기","통계","검색","종료" ]
+list("a+b")
+print(list("a+b"))
+c = ("번호를 선택하세요")
+d = ("2","3") 
+print("c",":", + "d'0") 
+print("="*46)
+print(f) 
+g = ["용 돈 기 록 장"]
+print(g) 
+print(f)
+
+a.items("번호":<5,"날짜":<7,"분류":<12,"내용":<14,"금액":<20)
+print("-"*46)
+print(i) 
+h = ("2026"."08","24".sep="-") 
+j = ("2026"."08","25".sep="-") 
+k = ("식비","교통","식비","문화") 
+l = ("식비","교통","식비","문화")
+
+dict_items([2026-08-24]_)
+import sys 
+args = sys.argv[1:]
+for i in args: 
+    print(i.upper(), end='') 
+import sys 
+if len(sys,args) < 2:
+    print("")  
