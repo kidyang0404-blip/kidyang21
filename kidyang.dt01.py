3f79f00d379971c0a69471f06ee67c759e7fc178
kidyang0404-blip <kidyang0404@gmail.com>
2026-09-14 16:44:42 +0900

    Add files via upload

---
+++ kidyang.dt01.py
@@ -0,0 +1,19 @@
+
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.dummy import DummyRegressor
+from sklearn.metrics import mean_absolute_error, r2_score
+
+X, y = load_diabetes(return_X_y= True)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = LinearRegression()
+model.fit(X_train, y_train) 
+
+pred = model.predict(X_test) 
+
+print("평균오차 =%6.2f" % mean_absolute_error(y_test,pred))
+print("설명력 =%7.4f" % r2_score(y_test,pred))
